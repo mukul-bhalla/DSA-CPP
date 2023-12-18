2d034283b284a724bcd7dcb3774ca9f15fb40614
mukul-bhalla <bhallamukul27@gmail.com>
2023-12-18 22:12:00 +0530

    Challenge 2

--- 07_Array/Challenges.cpp
+++ 07_Array/Challenges.cpp
@@ -9,16 +9,22 @@ private:
 public:
     Array()
     {
-        A = new int[5];
-        length = 5;
-        size = 5;
-        A[0] = 8;
-        A[1] = 9;
-        A[2] = 10;
-        A[3] = 12;
-        A[4] = 13;
+        A = new int[10];
+        length = 10;
+        size = 10;
+        A[0] = 7;
+        A[1] = 8;
+        A[2] = 9;
+        A[3] = 11;
+        A[4] = 12;
+        A[5] = 15;
+        A[6] = 16;
+        A[7] = 17;
+        A[8] = 18;
+        A[9] = 19;
     }
     int MissingElement1();
+    void MissingElementMore();
 };
 int Array ::MissingElement1()
 {
@@ -33,10 +39,28 @@ int Array ::MissingElement1()
     }
     return 0;
 }
+void Array::MissingElementMore()
+{
+    int num = length - 1;
+    int diff = A[0] - 0;
+    int i;
+    for (i = 1; i < length; i++)
+    {
+        if (A[i] - i != diff)
+        {
+            while (diff < A[i] - i)
+            {
+                cout << i + diff << " ";
+                diff++;
+            }
+        }
+    }
+}
 int main()
 {
     Array obj;
-    cout << obj.MissingElement1();
+    cout << obj.MissingElement1() << endl;
+    obj.MissingElementMore();
 
     return 0;
 }
