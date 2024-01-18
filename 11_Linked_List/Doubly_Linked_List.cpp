180295fb2c86fb0019824da42727b659ebf458ab
mukul-bhalla <bhallamukul27@gmail.com>
2024-01-19 02:00:36 +0530

    Insertion in doubly linked list

--- 11_Linked_List/Doubly_Linked_List.cpp
+++ 11_Linked_List/Doubly_Linked_List.cpp
@@ -43,11 +43,38 @@ int count(struct Node *p)
     }
     return x;
 }
+void Insert(struct Node *p, int index, int x)
+{
+    struct Node *t;
+    int i;
+    if (index < 0 || index > count(first))
+        return;
+    t = new Node;
+    t->data = x;
+    if (index == 0)
+    {
+        t->prev = NULL;
+        t->next = first;
+        first->prev = t;
+        first = t;
+    }
+    else
+    {
+        for (i = 0; i < index - 1; i++)
+            p = p->next;
+        t->next = p->next;
+        t->prev = p;
+        if (p->next)
+            p->next->prev = t;
+        p->next = t;
+    }
+}
 int main()
 {
     int A[] = {3, 4, 6, 7, 9};
     create(A, 5);
+    Insert(first, 5, 1);
     Display(first);
-    cout << count(first);
+    // cout << count(first);
     return 0;
 }
