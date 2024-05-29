f76c6e50b35a332aff810350cd6e87e02c9bf390
mukul-bhalla <bhallamukul27@gmail.com>
2024-05-29 17:47:26 +0530

    Selection Sort

--- 20_Sorting/AllSort.cpp
+++ 20_Sorting/AllSort.cpp
@@ -42,6 +42,23 @@ void insertionSort(int A[], int n)
         A[j + 1] = x;
     }
 }
+
+void selectionSort(int A[], int n)
+{
+    int i, j, k;
+    for (i = 0; i < n - 1; i++)
+    {
+        for (j = k = i; j < n; j++)
+        {
+            if (A[j] < A[k])
+            {
+                k = j;
+            }
+            swap(A[i], A[k]);
+        }
+    }
+}
+
 int main()
 {
     int A[] = {3, 7, 9, 10, 6, 5, 12, 4, 11, 2}, n = 10;
@@ -55,6 +72,7 @@ int main()
 
     // insertionSort(A, n);
 
+    selectionSort(A, n);
     for (int x : A)
     {
         cout << x << " ";
