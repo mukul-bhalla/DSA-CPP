e0548955284d6e045417b6896222620da2f875ba
mukul-bhalla <bhallamukul27@gmail.com>
2024-06-03 19:34:29 +0530

    Shell Sort

--- 20_Sorting/AllSort.cpp
+++ 20_Sorting/AllSort.cpp
@@ -190,6 +190,25 @@ void countSort(int A[], int n)
         }
     }
 }
+
+void shellSort(int A[], int n)
+{
+    int gap, i, j, temp;
+    for (gap = n / 2; gap >= 1; gap /= 2)
+    {
+        for (i = gap; i < n; i++)
+        {
+            temp = A[i];
+            j = i - gap;
+            while (j >= 0 && A[j] > temp)
+            {
+                A[j + gap] = A[j];
+                j = j - gap;
+            }
+            A[j + gap] = temp;
+        }
+    }
+}
 int main()
 {
     int A[] = {3, 7, 9, 10, 6, 5, 12, 4, 11, 2}, n = 10;
@@ -211,8 +230,9 @@ int main()
 
     // RmergeSort(A, 0, n);
 
-    countSort(A, n);
+    // countSort(A, n);
 
+    shellSort(A, n);
     for (int x : A)
     {
         cout << x << " ";
